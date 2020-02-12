c9676ae113b6592be8da1b8e8e1f3d24dfb1e316
munar <munar2@hotmail.com>
2020-02-12 21:13:29 +0100

    start-models-controlers-migrations

---
+++ dreadflockLaravel/app/Http/Controllers/HabilitiesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Habilities;
+use Illuminate\Http\Request;
+
+class HabilitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Habilities  $habilities
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Habilities $habilities)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Habilities  $habilities
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Habilities $habilities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Habilities  $habilities
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Habilities $habilities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Habilities  $habilities
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Habilities $habilities)
+    {
+        //
+    }
+}
